049ba1b37259e4222c1e382a125dc7acb8d18765
Jonas Ångström <jonas.angstrom@gradientech.se>
2024-04-21 11:53:17 +0200

    some updates

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -38,7 +38,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     //Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &app_address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -67,7 +67,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             .expect("Failed to execute request.");
 
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
