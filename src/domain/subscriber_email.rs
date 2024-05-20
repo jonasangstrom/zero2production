1d6872fb2d05c5ac5bcbf20e0876597e6fc7e916
Jonas Ångström <jonas.aangstroem@gmail.com>
2024-05-20 22:19:02 +0200

    using fake to generate random ok email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -20,6 +20,8 @@ impl SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
@@ -37,12 +39,12 @@ mod tests {
     }
     #[test]
     fn nice_email() {
-        let email = "man@domain.com".to_string();
+        let email: String = SafeEmail().fake();
 
-        let parsed_email = match SubscriberEmail::parse(email) {
+        let parsed_email = match SubscriberEmail::parse(email.clone()) {
             Ok(pe) => pe,
             Err(_) => panic!(),
         };
-        assert_eq!(parsed_email.as_ref(), &"man@domain.com".to_string());
+        assert_eq!(parsed_email.as_ref(), &email);
     }
 }
